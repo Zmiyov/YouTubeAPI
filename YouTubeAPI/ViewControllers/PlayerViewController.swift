ba658e07d5ec19a4a89b96086dc143f9cb23f617
Volodymyr <zmiyov88@gmail.com>
2022-10-08 15:06:02 +0300

    Video player appears

--- YouTubeAPI/ViewControllers/PlayerViewController.swift
+++ YouTubeAPI/ViewControllers/PlayerViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import YouTubePlayerKit
 
 class PlayerViewController: UIViewController {
 
     @IBOutlet var handleArea: UIView!
-    
     @IBOutlet var openCloseButton: UIButton!
     
+    @IBOutlet var videoView: UIView!
+    
     @IBOutlet var timeLineSlider: UISlider!
+    @IBOutlet var recentTameLabel: UILabel!
+    @IBOutlet var fullTimeLabel: UILabel!
+    
+    @IBOutlet var videoNameLabel: UILabel!
+    @IBOutlet var amountOfViewsLabel: UILabel!
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,16 +31,35 @@ class PlayerViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+        configureGradientLayer()
         let timelineSliderThumbImage = UIImage(named: "Line.png")
         timeLineSlider.setThumbImage(timelineSliderThumbImage, for: .normal)
-        configureGradientLayer()
+        
+        
+            
+        addVideoPlayerView()
+        
+
     }
     
     
     @IBAction func openCloseButtonTapped(_ sender: UIButton) {
 
     }
+    
+    func addVideoPlayerView() {
+//        let youTubePlayer = YouTubePlayer(stringLiteral: "https://www.youtube.com/watch?v=w87fOAG8fjk")
+        let configuration = YouTubePlayer.Configuration(
+            autoPlay: true,
+            showControls: false,
+            loopEnabled: true
+        )
+//        let youTubePlayer = YouTubePlayer(source: .playlist(id: "PLHFlHpPjgk706qEJf9fkclIhdhTkH49Tb"), configuration: configuration)
+        let hostingView = YouTubePlayerHostingView(source: .playlist(id: "PLHFlHpPjgk706qEJf9fkclIhdhTkH49Tb"), configuration: configuration)
+        hostingView.frame = videoView.bounds
+        videoView.addSubview(hostingView)
+    }
+    
     
     func configureGradientLayer() {
         view.backgroundColor = .clear
