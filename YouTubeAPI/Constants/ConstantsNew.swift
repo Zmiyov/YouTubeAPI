106c4a3dac5e3116cc4402db8a483ea44f29c4a2
Volodymyr <zmiyov88@gmail.com>
2022-10-17 10:05:07 +0300

    Create new file with constants.

---
+++ YouTubeAPI/Constants/ConstantsNew.swift
@@ -0,0 +1,17 @@
+//
+//  ConstantsNew.swift
+//  YouTubeAPI
+//
+//  Created by Vladimir Pisarenko on 17.10.2022.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let apiKey = "AIzaSyA7m_fsYx4fSM4MrvW03Uxd6dPjziMBagM"
+    
+    static var iosAcadememyPlaylistId = "PL5PR3UyfTWvc6DLxny-s7rcClqEGjqLfN"
+    static var infoCarPlaylistId = "PLtItvIWNIaRhgxA-YYuGCDtcJScVam5og"
+    
+}
